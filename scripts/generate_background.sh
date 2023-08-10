369747b0deb8b3906b64c9762cbcd6191b4e19dd
vivian-yan219 <71372773+vivian-yan219@users.noreply.github.com>
2023-08-10 14:41:22 +0800

    Update generate_background.sh
    delete api key details

--- scripts/generate_background.sh
+++ scripts/generate_background.sh
@@ -6,4 +6,4 @@ python3 self_instruct/self_background_gpt.py \
     --seed_tasks_path data/gpt3_seeds.json \
     --engine_chat "gpt-3.5-turbo" \
     --engine_new "davinci" \
-    --api_key "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"
+    --api_key ""
